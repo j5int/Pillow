dde12fb9462c8476e21dc49ba731ee4abe0f95e3
Jeremy <jeremy.westwood@hexagon.com>
2021-07-28 11:12:55 +0200

    comment out the python versions we don't need to try and build

--- winbuild/config.py
+++ winbuild/config.py
@@ -5,12 +5,12 @@ GITHUB_DEPENDS_URL = "https://github.com/python-pillow/pillow-depends/blob/maste
 
 pythons = {
     "27": {"compiler": 7, "vc": 2010},
-    "pypy2": {"compiler": 7, "vc": 2010},
-    "35": {"compiler": 7.1, "vc": 2015},
-    "36": {"compiler": 7.1, "vc": 2015},
-    "pypy3": {"compiler": 7.1, "vc": 2015},
-    "37": {"compiler": 7.1, "vc": 2015},
-    "38": {"compiler": 7.1, "vc": 2015},
+    # "pypy2": {"compiler": 7, "vc": 2010},
+    # "35": {"compiler": 7.1, "vc": 2015},
+    # "36": {"compiler": 7.1, "vc": 2015},
+    # "pypy3": {"compiler": 7.1, "vc": 2015},
+    # "37": {"compiler": 7.1, "vc": 2015},
+    # "38": {"compiler": 7.1, "vc": 2015},
 }
 
 VIRT_BASE = "c:/vp/"
